4b1ea98f5df63f4f1d2a6fa36f1c2804c5774482
Jesper Kristensen <cableman@linuxdev.dk>
2014-01-28 10:52:20 +0100

    Added the first socket.io connection test

--- app.js
+++ app.js
@@ -12,21 +12,29 @@ var routes = require('./routes');
 
 var app = express();
 var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+var sio = require('socket.io').listen(server);
 
-
-server.listen(3000)
-io.set('loglevel',10) // set log level to get all debug messages
-
-io.on('connection',function(socket){
-  socket.emit('init', {msg:"test"})
-})
-
+// Set express app configuration.
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
-
+app.use(express.favicon());
+app.use(express.logger('dev'));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Start the server.
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+})
+
+// Configure socket.
+sio.set('loglevel',10);
+
+// Handle socket events.
+sio.on('connection',function(socket){
+  socket.emit('init', {msg:"test"})
+})
+
+// Set express routes.
 app.get('/', routes.index);
