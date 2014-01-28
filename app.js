d50ddb76b7e5d092769fd0152be4f8eab6819038
Jesper Kristensen <cableman@linuxdev.dk>
2014-01-28 15:47:43 +0100

    Got token based socket.io auth working

--- app.js
+++ app.js
@@ -16,12 +16,18 @@ var app = express();
 var server = http.createServer(app);
 var sio = require('socket.io').listen(server);
 
+// Token based auth.
+var socketio_jwt = require('socketio-jwt');
+var jwt = require('jsonwebtoken');
+var jwt_secret = 'foo bar big secret';
+
 // Set express app configuration.
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
@@ -30,10 +36,32 @@ server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 })
 
+sio.configure(function (){
+  sio.set('authorization', socketio_jwt.authorize({
+    secret: jwt_secret,
+  	handshake: true
+  }))
+});
+
 // Handle socket events.
 sio.on('connection', function(socket) {
+  console.log(socket.handshake.decoded_token.email, 'connected');
   socket.emit('init', {msg:"test"});
 })
 
 // Set express routes.
 app.get('/', routes.index);
+
+// Login request.
+app.post('/login', function (req, res) {
+  
+  var profile = {
+    username: req.body.username,
+    password: req.body.password
+  };
+
+  // We are sending the profile inside the token
+  var token = jwt.sign(profile, jwt_secret, { expiresInMinutes: 60*5 });
+
+  res.json({token: token});
+});
--- public/javascripts/stander.js
+++ public/javascripts/stander.js
@@ -1,4 +1,19 @@
-var socket = io.connect('http://127.0.0.1:3000');
-socket.on('init', function (data) {
-  console.log(data);
+
+$.ajax({
+  type: 'POST',
+  data: {
+    username: 'test',
+	password: 'password'
+  },
+  url: '/login'
+}).done(function (result) {
+  var sio = io.connect('http://127.0.0.1:3000', { query: 'token=' + result.token });
+
+  sio.socket.on('error', function (reason){
+    console.error('Unable to connect Socket.IO', reason);
+  });
+
+  sio.on('init', function (data) {
+    console.log(data);
+  });
 });
