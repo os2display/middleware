7848c31ba8c092839cb07c7b7dd68a44db625245
Jesper Kristensen <cableman@linuxdev.dk>
2014-12-12 12:51:04 +0100

    Issue IKTRE-103 by cableman: Added the basic core for the proxy as plugins

---
+++ app.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+/**
+ * @file
+ * This is the main application that uses architect to build the application
+ * base on plugins.
+ */
+
+var path = require('path');
+var architect = require("architect");
+
+// Load config file.
+var configs = require(__dirname + '/config.json');
+
+// Configure the plugins.
+var config = [
+  {
+    "packagePath": "./plugins/logger",
+    "filename": configs.log.file,
+    "debug": configs.log.debug
+  },
+  {
+    "packagePath": "./plugins/server",
+    "port": configs.port,
+    "path": path.join(__dirname, 'public')
+  },
+  {
+    "packagePath": "./plugins/socket"
+  }
+];
+
+// User the configuration to start the application.
+config = architect.resolveConfig(config, __dirname);
+architect.createApp(config, function (err, app) {
+  if (err) {
+    throw err;
+  }
+});
