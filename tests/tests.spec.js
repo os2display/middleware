15ffa17d149ff553624f74d2ab8355f7a7f47248
Jesper Kristensen <cableman@linuxdev.dk>
2014-12-12 12:41:06 +0100

    Issue IKTRE-103 by cableman: Removed all the old code

--- tests/tests.spec.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file
- * This is the main test file, which includes the other tests in the right order.
- */
-
-require('./configuration.spec');
-require('./logger.spec');
-require('./cache.spec');
