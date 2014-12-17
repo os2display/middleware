0c2bc146c6cbca155308377f5a05491f32e0cc7d
Jesper Kristensen <cableman@linuxdev.dk>
2014-12-17 18:36:28 +0100

    Issue IKTREDEL-103 by cableman: Refactored the client plugin to be simpler

--- plugins/client/client.js
+++ plugins/client/client.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * Handle socket communication with the client (frontend).
+ * Handle initial socket communication with the client (frontend).
  */
 
 /**
@@ -12,51 +12,10 @@ module.exports = function (options, imports, register) {
   // Load promise library.
   var Q = require('q');
 
-  // Activate client placeholder.
-  var clients = {};
-
   // Injections.
   var socket = imports.socket;
   var Screen = imports.screen;
   var logger = imports.logger;
-
-  /**
-   * Try to get screen information.
-   *
-   * @param apikey
-   * @param id
-   * @returns {*}
-   */
-  function getScreen(apikey, id) {
-    var deferred = Q.defer();
-
-    // Check if API key index exists.
-    if (!clients.hasOwnProperty(apikey)) {
-      // Add API key.
-      clients[apikey] = {};
-    }
-
-    // Check if screen exists.
-    if (!clients[apikey].hasOwnProperty(id)) {
-      // Try to load the screen.
-      var screen = new Screen(apikey, id);
-      screen.load().then(
-        function () {
-          // Store screen in static cache.
-          clients[apikey][id] = screen;
-          deferred.resolve(clients[apikey][id]);
-        },
-        function (error) {
-          deferred.reject(error);
-        }
-      );
-    }
-    else {
-      deferred.resolve(clients[apikey][id]);
-    }
-
-    return deferred.promise;
-  }
 
   /**
    * Handle socket connection event from a client.
@@ -70,17 +29,31 @@ module.exports = function (options, imports, register) {
      */
     socket.on('ready', function (state) {
       // Try to get the screen.
-      getScreen(profile.apikey, profile.screenID).then(
-        function (screen) {
-          // Send a 200 ready code back to the client.
-          socket.emit('ready', {
-            "statusCode": 200
-          });
+      var screen = new Screen(profile.apikey, profile.screenID);
+      screen.load().then(
+        function (obj) {
+          obj.title = profile.screenTitle;
+          obj.save().then(
+            function () {
+              // Send a 200 ready code back to the client.
+              socket.emit('ready', {
+                "statusCode": 200
+              });
 
-          /**
-           * @TODO: Push content if content is ready.
-           */
-          // Loop over all channels and push content to the screen.
+              /**
+               * @TODO: Push content if content is ready.
+               *
+               * Loop over all channels and push content to the screen.
+               */
+            },
+            function (error) {
+              // Send error to client.
+              socket.emit('error', {
+                "statusCode": 500,
+                "message": error.message
+              });
+            }
+          );
         },
         function (error) {
           // Send error to client.
@@ -97,9 +70,5 @@ module.exports = function (options, imports, register) {
   });
 
   // Register the plugin with the system.
-  register(null, {
-    "client" : {
-      'getScreen': getScreen
-    }
-  });
+  register(null, null);
 };
